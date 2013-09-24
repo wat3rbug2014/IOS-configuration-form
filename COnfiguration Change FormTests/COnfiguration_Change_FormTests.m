74846f6b4bfb4ce86f44111477eced01845c6649
Douglas Gardiner <doug.gardiner@gmail.com>
2013-09-24 12:21:42 -0500

    Initial Commit

---
+++ COnfiguration Change FormTests/COnfiguration_Change_FormTests.m
@@ -0,0 +1,34 @@
+//
+//  COnfiguration_Change_FormTests.m
+//  COnfiguration Change FormTests
+//
+//  Created by Douglas Gardiner on 9/24/13.
+//  Copyright (c) 2013 Douglas Gardiner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface COnfiguration_Change_FormTests : XCTestCase
+
+@end
+
+@implementation COnfiguration_Change_FormTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
