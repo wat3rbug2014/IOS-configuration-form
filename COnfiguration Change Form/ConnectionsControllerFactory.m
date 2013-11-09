1c4ee4378107f1aaf5ea84e99c944a8ab983499b
Douglas Gardiner <doug.gardiner@gmail.com>
2013-11-09 10:01:55 -0600

    Add Factory class for creating Connections since there will be many types.

---
+++ COnfiguration Change Form/ConnectionsControllerFactory.m
@@ -0,0 +1,24 @@
+//
+//  ConnectionsControllerFactory.m
+//  COnfiguration Change Form
+//
+//  Created by Douglas Gardiner on 11/9/13.
+//  Copyright (c) 2013 Douglas Gardiner. All rights reserved.
+//
+
+#import "ConnectionsControllerFactory.h"
+
+@implementation ConnectionsControllerFactory
+
+-(ConnectionsController*) createConnectionsController: (int) formType {
+    
+    switch (formType) {
+        case ADD: return [[ConnectionsController alloc] init];
+            break;
+        default: return [[ConnectionsController alloc] init];
+            break;
+        
+    }
+}
+
+@end
