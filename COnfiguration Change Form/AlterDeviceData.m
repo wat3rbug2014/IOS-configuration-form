11716dcf1143ac660365328aff96184fe0976699
Douglas Gardiner <doug.gardiner@gmail.com>
2014-05-19 14:09:01 -0500

    fixed red check for form on alter config

--- COnfiguration Change Form/AlterDeviceData.m
+++ COnfiguration Change Form/AlterDeviceData.m
@@ -21,7 +21,8 @@
     
     // basic information about device
     
-    BOOL result = true;;
+    BOOL result = true;
+    BOOL isAltered = false;
     if([[super tag] length] == 0) {
         result = false;
     }
@@ -36,32 +37,30 @@
     }
     // check to see that some kind of connection is different
     
-    if ([[super uplinkOneTo] length] > 0 || [super vlan] > 0 || [[super uplinkTwoTo] length] > 0) {
-        
-        // check for complete uplink 1 info
-        
-        if ([[super uplinkOneTo] length] > 0) {
-            if ([[super uplinkOneFrom] length] == 0 || [[super destOneTag] length] == 0) {
-                result = false;
-            }
-        }
-        
-        // check for complete uplink 2 info
-        
-        if ([[super uplinkTwoTo] length] > 0) {
-            if ([[super uplinkTwoFrom] length] == 0 || [[super destTwoTag] length] == 0) {
-                result = false;
-            }
-        }
-        // if vlan is different the IP address has to be different.
-        
-        if ([super vlan] > 0) {
-            if ([[super ipAddress] length] == 0) {
-                result = false;
-            }
+    if ([[super uplinkOneTo] length] > 0 || [[super uplinkOneFrom] length] > 0 || [[super destOneTag] length] > 0) {
+        isAltered = true;
+        if ([[super uplinkOneTo] length] <= 0 || [[super uplinkOneFrom] length] <= 0 || [[super destOneTag] length] <= 0) {
+            result = false;
         }
     }
-    return result;
+    if ([[super uplinkTwoTo] length] > 0 || [[super uplinkTwoFrom] length] > 0 || [[super destTwoTag] length] > 0) {
+        isAltered = true;
+        if ([[super uplinkTwoTo] length] <= 0 || [[super uplinkTwoFrom] length] <= 0 || [[super destTwoTag] length] <= 0) {
+            result = false;
+        }
+    }
+    // if vlan is different the IP address has to be different.
+        
+    if ([super vlan] > 0) {
+        isAltered = true;
+        if ([[super ipAddress] length] == 0) {
+            result = false;
+        }
+    }
+    if ([[super ipAddress] length] > 0) {
+        isAltered = true;
+    }
+    return result && isAltered;
 }
 
 -(NSString*) getEmailMessageBody {
