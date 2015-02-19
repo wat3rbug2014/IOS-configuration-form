dcf22621f9cef0a793fb55f5ccfd9fa06b5aa171
Douglas Gardiner <doug.gardiner@gmail.com>
2015-02-19 09:26:02 -0600

    Add documentation for the factory class.

--- COnfiguration Change Form/FormViewControllerFactory.h
+++ COnfiguration Change Form/FormViewControllerFactory.h
@@ -6,6 +6,13 @@
 //  Copyright (c) 2013 Douglas Gardiner. All rights reserved.
 //
 
+/**
+ * This class is a factory class for creating the view controllers for the tab 
+ * view controller for the application.  It is meant as a central point of creation
+ * so that polymorphic behavior can be used.  See enumList.h for connectType enum values
+ * that can be passed to this class.
+ */
+
 #import "FormViewController.h"
 #import "AddDeviceController.h"
 #import "RemoveDeviceController.h"
@@ -15,6 +22,15 @@
 
 @interface FormViewControllerFactory : NSObject
 
+
+/**
+ * Creates a view controller based on integer values passed to it.  See enumList.h for connectType
+ * enum values for the acceptable range of values to pass to this method.
+ *
+ * @param type the enum value used for selecting which view controller to create.
+ *
+ * @return A subclass of the FormViewController class.
+ */
 
 +(id) createFormView:(int) type;
 
